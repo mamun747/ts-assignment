94bf782463e199d0c072d28843127189e8371cd8
mamun747 <technoultra42@gmail.com>
2025-01-03 18:58:49 +0600

    task adding functionality were included

--- Task-Management/src/App.tsx
+++ Task-Management/src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import "react-toastify/dist/ReactToastify.css"; // Import the toast styles
-import { AiOutlineDelete } from "react-icons/ai";
-import { FaRegEdit } from "react-icons/fa";
-import { IoMdCheckmarkCircle, IoMdCheckmarkCircleOutline } from "react-icons/io";
-import { toast, ToastContainer } from "react-toastify";
+import React, { useState } from "react";
+// import "react-toastify/dist/ReactToastify.css"; // Import the toast styles
+// import { AiOutlineDelete } from "react-icons/ai";
+// import { FaRegEdit } from "react-icons/fa";
+// import { IoMdCheckmarkCircle, IoMdCheckmarkCircleOutline } from "react-icons/io";
+// import { toast, ToastContainer } from "react-toastify";
 
 interface value {
   id: number;
@@ -21,9 +21,9 @@ function App() {
     e.preventDefault();
     const value = newInput.trim();
     setInput([...input, { id: Date.now(), text: value, completed: false }]);
-    toast.success("You successfully added a task! Let complete it!", {
-      position: "top-center",
-    });
+    // toast.success("You successfully added a task! Let complete it!", {
+    //   position: "top-center",
+    // });
     setNewInput("");
   };
   const handleEdit = (e: value) => {
@@ -53,6 +53,17 @@ function App() {
   };
   return (
     <>
+     <div className="bg-slate-200 w-full h-screen p-5 flex justify-center items-center">
+      <div>
+        <input type="text" value={newInput} onChange={(e) => setNewInput(e.target.value)} className="mx-auto border rounded-xl p-2 w-[500px] outline-none" autoFocus/>
+        <button onClick={handleClick} disabled={!newInput.trim()} className="p-2 bg-purple-400/50 rounded-lg ms-2 font-bold disabled:cursor-not-allowed disabled:opacity-50">ADD</button>
+        {
+          input.map(value => (
+            <p key={value.id}>{value.text}</p>
+          ))
+        }
+      </div>
+    </div>
 
     </>
   )
