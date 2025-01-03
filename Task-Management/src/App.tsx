8314d6906293c0e4e247c0bcb402ff52cae57393
mamun747 <technoultra42@gmail.com>
2025-01-03 17:11:36 +0600

    all states are defined and important elements are imported

--- Task-Management/src/App.tsx
+++ Task-Management/src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import React, { useState, useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css"; // Import the toast styles
+import { AiOutlineDelete } from "react-icons/ai";
+import { FaRegEdit } from "react-icons/fa";
+import { IoMdCheckmarkCircle, IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { toast, ToastContainer } from "react-toastify";
 
+interface value {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+function App() {
+  const [input, setInput] = useState<value[]>([]);
+  const [newInput, setNewInput] = useState("");
+  const [editingId, setEditingId] = useState<number | null>();
+  const [editingText, setEditingText] = useState<string>("");
   return (
     <>
+
     </>
   )
 }
 
-export default App
+export default App;
